370379b1acfe4c381ef2b1f16aa02e1f84df06b2
Debobrata Pal <dev.debobratapal@gmail.com>
2024-09-13 23:39:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Print GFG n times/print-gfg-n-times.java
+++ Difficulty: Easy/Print GFG n times/print-gfg-n-times.java
@@ -22,10 +22,12 @@ class Main {
 class Solution {
 
     void printGfg(int N) {
-        if (N == 0){
+        // code here
+        if(N == 0) {
             return;
         }
-        System.out.print("GFG ");
+        
         printGfg(N - 1);
+        System.out.print("GFG ");
     }
 }
