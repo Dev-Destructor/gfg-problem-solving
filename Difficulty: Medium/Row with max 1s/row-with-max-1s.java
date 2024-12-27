a5607f666f780459bc7a51dcc454831c3fb8a719
Debobrata Pal <dev.debobratapal@gmail.com>
2024-12-27 14:49:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Row with max 1s/row-with-max-1s.java
@@ -0,0 +1,87 @@
+//{ Driver Code Starts
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt(); // Number of test cases
+        sc.nextLine();        // Consume the newline character
+
+        while (t-- > 0) {
+            String input = sc.nextLine();
+
+            // Replace ][ with ],[
+            input = input.replace("][", "],[");
+
+            ArrayList<ArrayList<Integer>> mat = new ArrayList<>();
+            String[] rows = input.split("],\\s*\\[");
+
+            for (String row : rows) {
+                row = row.replaceAll("[\\[\\]]", ""); // Remove any surrounding brackets
+                ArrayList<Integer> intRow = new ArrayList<>();
+                for (String num : row.split(",")) {
+                    intRow.add(Integer.parseInt(num.trim()));
+                }
+                mat.add(intRow);
+            }
+
+            Solution obj = new Solution();
+            int result = obj.rowWithMax1s(convertListToArray(mat));
+
+            System.out.println(result);
+
+            System.out.println("~");
+        }
+        sc.close();
+    }
+
+    // Helper method to convert ArrayList<ArrayList<Integer>> to 2D array
+    public static int[][] convertListToArray(ArrayList<ArrayList<Integer>> mat) {
+        int rows = mat.size();
+        int cols = mat.get(0).size();
+        int[][] arr = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                arr[i][j] = mat.get(i).get(j);
+            }
+        }
+        return arr;
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int rowWithMax1s(int arr[][]) {
+        int n = arr.length;
+        int maxCnt = 0;
+        int maxI = -1;
+        // code here
+        for(int i = 0; i < n; i++) {
+            int x = arr[i].length;
+            int l = 0, h = x - 1, fo = x;
+            
+            while(l <= h) {
+                int m = (l + h) / 2;
+                
+                if(arr[i][m] == 1) {
+                    fo = m;
+                    h = m - 1;
+                } else {
+                    l = m + 1;
+                }
+            }
+            
+            int cnt = x - fo;
+            if(cnt > maxCnt) {
+                maxCnt = cnt;
+                maxI = i;
+            }
+        }
+        
+        return maxI;
+    }
+}
